0a4760d957a3ab4f957485f3f146d7eada2537db
paulmwatson <github.com@paulmwatson.com>
2022-08-25 23:23:02 +0200

    Basic e2e testing with Cypress

---
+++ cypress/e2e/index.cy.js
@@ -0,0 +1,8 @@
+import common from "/public/locales/en/common.json";
+
+describe("home spec", () => {
+  it("passes", () => {
+    cy.visit("/").contains(common.home.h1);
+    cy.visit("/").contains(common.home.intro);
+  });
+});
