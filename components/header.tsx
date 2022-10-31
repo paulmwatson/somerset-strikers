38b4e82e8ae2a9b93a775270b92974b8e6148a1b
paulmwatson <github.com@paulmwatson.com>
2022-10-31 12:59:53 +0200

    Join page content

---
+++ components/header.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Head from "next/head";
+import { useTranslation } from "next-i18next";
+
+export default function Header() {
+  const { t } = useTranslation();
+  return (
+    <Head>
+      <title>{t("site.title")}</title>
+      <meta name="description" content={t("site.description")} />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="favicon-16x16.png"
+      />
+
+      <link rel="manifest" href="site.webmanifest" />
+      <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff" />
+
+      <meta
+        property="og:image"
+        content="https://www.somersetstrikers.co.za/profile.webp"
+      />
+      <meta property="og:url" content="https://www.somersetstrikers.co.za/" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={t("site.title")} />
+      <meta property="og:description" content={t("site.description")} />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="somersetstrikers.co.za" />
+      <meta
+        property="twitter:url"
+        content="https://www.somersetstrikers.co.za/"
+      />
+      <meta name="twitter:title" content={t("site.title")} />
+      <meta name="twitter:description" content={t("site.description")} />
+      <meta
+        name="twitter:image"
+        content="https://www.somersetstrikers.co.za/profile.webp"
+      />
+    </Head>
+  );
+}
---
+++ components/navigation.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import React from "react";
+import Link from "next/link";
+import { useTranslation } from "next-i18next";
+
+export default function Navigation() {
+  const { t } = useTranslation();
+  return (
+    <header>
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <Link href="/">
+            <a className="navbar-item">
+              <Image
+                src="/logo-wide.webp"
+                alt="Logo"
+                height="128"
+                width="472"
+              />
+            </a>
+          </Link>
+        </div>
+
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <Link href="/join">
+              <a className="navbar-item is-hidden">{t("navigation.join")}</a>
+            </Link>
+            <Link href="/">
+              <a className="navbar-item is-hidden">{t("navigation.practice")}</a>
+            </Link>
+            <Link href="/">
+              <a className="navbar-item is-hidden">{t("navigation.play")}</a>
+            </Link>
+          </div>
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <Link href="/contact-us">
+                  <a className="button is-primary is-outlined">
+                    <strong>{t("navigation.contact-us")}</strong>
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
