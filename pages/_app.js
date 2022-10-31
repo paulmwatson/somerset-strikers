4674997cfec3228c6346b3eaede0ebda3f48abff
paulmwatson <github.com@paulmwatson.com>
2022-10-31 15:52:40 +0200

    Analytics Vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import { appWithTranslation } from "next-i18next";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default appWithTranslation(MyApp);
