85a22b5b45203f09d52d9f365fcc1c7996727f50
paulmwatson <github.com@paulmwatson.com>
2023-01-18 11:06:06 +0200

    Update test

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -4,6 +4,6 @@ describe("home spec", () => {
   it("passes", () => {
     cy.visit("/").contains(common.home.h1);
     cy.visit("/").contains(common.home.intro);
-    cy.visit("/").contains(common.home.register_your_interest);
+    cy.visit("/").contains(common.home.join);
   });
 });
