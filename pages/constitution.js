0d32c029f2e3df6c498621433ce1880187c55025
paulmwatson <github.com@paulmwatson.com>
2024-10-28 12:48:03 +0200

    Merge proposed constitution in

--- pages/constitution.js
+++ pages/constitution.js
@@ -18,35 +18,39 @@ const Page = () => {
           <section className="section content">
             <h1 className="title has-text-centered">Constitution</h1>
             <h2 className="title">1. NAME</h2>
-
             <p>
               The name of the Cricket Club shall be Somerset Strikers and shall
               be referred to Hereafter as the Club.
             </p>
-
             <h2 className="title">2. HEADQUARTERS</h2>
             <p>
               The administrative headquarters of the Club shall be at 3 Seaview
               Road, Somerset West, 7130.
             </p>
-
             <h2 className="title">3. OBJECTS</h2>
             <p>The objects, intents and purposes of the Club shall be:</p>
             <p>
-              3.1. To promote the development of junior cricket in the Somerset
+              3.1 To function as a recreational club of which the sole or
+              principal object is to provide social and recreational amenities
+              or facilities to its members by:
+            </p>
+            <p>
+              3.1.1. Promoting the development of junior cricket in the Somerset
               West area by creating a safe, constructive, and competitive
               environment in which children can develop as cricketers, and
               people, with the assistance of committed and responsible parents.
             </p>
             <p>
-              3.2. To be an inclusive community junior cricket club with a goal
-              of catering for all junior players wanting to play competitive
-              cricket.
+              3.1.2. Being an inclusive community junior cricket club with a
+              goal of catering for all junior players wanting to play
+              competitive cricket.
             </p>
-
+            <p>
+              The Club is to carry on its activities in a non-profit manner.
+            </p>
             <h2 className="title">4. MANAGEMENT</h2>
             <p>
-              The management, control and administration of all matters and
+              4.1. The management, control and administration of all matters and
               affairs of the Club shall be vested in a controlling body,
               hereafter referred to as the Committee and which shall be composed
               as follows:
@@ -59,12 +63,36 @@ const Page = () => {
               <li>Honorary Secretary</li>
             </ul>
             <p>
-              The Committee shall have the power to accept, refuse and cancel
-              membership of the Club. The Committee and any sub-committee will
-              be made up of volunteers, no remuneration will be paid to these
-              positions.
+              4.2. The Committee shall have the power to accept, refuse, and
+              cancel membership in the Club. The Committee and any subcommittee
+              will be made up of volunteers; no remuneration will be paid to
+              these positions.
             </p>
-
+            <p>4.3. All Committee members must be natural persons.</p>
+            <p>
+              4.4. At least 3 (three) members of the Committee shall be
+              unrelated to each other to the third degree of consanguinity.
+            </p>
+            <p>
+              4.5. The members of the Committee accept fiduciary responsibility
+              for the Club.
+            </p>
+            <p>
+              4.6. Any person who is disqualified, in terms of South African
+              legislation<sup>[1]</sup>, from acting in a fiduciary capacity may
+              not be elected as a member of the Committee. Should an acting
+              Committee member, in terms of South African legislation, be
+              disqualified from acting in a fiduciary capacity, that person
+              shall immediately cease to be a Committee member.
+            </p>
+            <p>
+              <small>
+                [1] Specifically, section 6 of the Trust Property Control Act
+                No. 57 of 1988, section 25A of the Nonprofit Organisations Act
+                No. 71 of 1997 or section 69 of the Companies Act No. 71 of
+                2008.
+              </small>
+            </p>
             <h2 className="title">5. POWERS AND DUTIES OF COMMITTEE MEMBERS</h2>
             <h3 className="title">5.1 CHAIRPERSON</h3>
             <p>
@@ -88,7 +116,6 @@ const Page = () => {
               5.1.6. They shall be an authorised signatory and must co-sign any
               authorised payments
             </p>
-
             <h3 className="title">5.2 VICE CHAIRPERSON</h3>
             <p>5.2.1. To assist the Chairperson in fulfilling their duties.</p>
             <p>
@@ -101,7 +128,6 @@ const Page = () => {
               new member - including introducing them to their age group
               manager.
             </p>
-
             <h3 className="title">5.3 CLUB CAPTAIN</h3>
             <p>
               5.3.1. The Club Captain shall act as the intermediary between a
@@ -112,7 +138,6 @@ const Page = () => {
               grievance, or part grievance, of a member and report back to the
               member on the action taken or decision made by the Committee.
             </p>
-
             <h3 className="title">5.4 HONORARY TREASURER</h3>
             <p>
               5.4.1. The Honorary Treasurer shall have the control of the
@@ -126,16 +151,15 @@ const Page = () => {
               5.4.2. They shall act ex-officio on any sub-committee which may be
               concerned with income and/or expenditure. They shall be empowered
               to pay all authorised and routine accounts and to submit proper
-              books of accounts for auditing once seasonally before the Clubs
-              AGM, with an unaudited review mid season and shall cause the
-              preparation of a financial statement for presentation to the
-              Annual General Meeting.
+              books of accounts for auditing once seasonally before the{" "}
+              Club&apos;s AGM, with an unaudited review mid-season and shall
+              cause the preparation of a financial statement for presentation to
+              the Annual General Meeting.
             </p>
             <p>
               5.4.3. They will be an authorised signatory and must co-sign any
               authorised payments
             </p>
-
             <h3 className="title">5.5 HONORARY SECRETARY</h3>
             <p>
               5.5.1. To conduct all correspondence under the direction of the
@@ -159,7 +183,6 @@ const Page = () => {
               5.5.5. To perform all duties relevant to the Secretary&apos;s
               Office.
             </p>
-
             <h2 className="title">6. ADDITIONAL POWERS</h2>
             <p>
               In addition to their duties, members of the Committee shall be
@@ -195,7 +218,6 @@ const Page = () => {
               6.6. To hear, to deal with, consider and decide upon appeals or
               complaints by bona fide members.
             </p>
-
             <p>
               6.7. No member having been suspended shall be reinstated without
               the consent of the Committee.
@@ -245,11 +267,12 @@ const Page = () => {
             </p>
             <p>
               7.2. All members of the Club shall be duly registered in the
-              members&apos; register of the Club.
+              members&apos; register of the Club and shall be entitled to annual
+              or seasonal membership .
             </p>
             <p>
               7.3. Any junior candidate for admission to the Club shall require
-              a parent / guardian to complete the prescribed online registration
+              a parent/guardian to complete the prescribed online registration
               form.
             </p>
             <p>
@@ -266,6 +289,10 @@ const Page = () => {
               subscription fees may be increased at a forthcoming Annual or
               Special meeting of the Club.
             </p>
+            <p>
+              7.6 Members may not sell or transfer any rights attached to their
+              membership.
+            </p>
             <h2 className="title">8. PLAYING & SOCIAL MEMBERS</h2>
             <p>
               Playing members shall be all those members who are, at the date
@@ -281,7 +308,6 @@ const Page = () => {
               committee. Deadline for this request will be by the end of the
               calendar year and adjusted fees will be applied.
             </p>
-
             <h2 className="title">9. SELECTION POLICY</h2>
             <p>
               The Selection Policy will meet the expectations and playing needs
@@ -295,7 +321,6 @@ const Page = () => {
               <Link href="/policies/selection">“Selection Policy”</Link>{" "}
               document for a full explanation on the Selection Policy.
             </p>
-
             <h2 className="title">10. ANTI-DISCRIMINATION POLICY</h2>
             <p>
               The Club strives to be an inclusive club. Discrimanation against
@@ -309,12 +334,10 @@ const Page = () => {
               </Link>{" "}
               document for the detailed policy.
             </p>
-
             <h2 className="title">11. FINANCIAL YEAR</h2>
             <p>
               The financial year shall run from May through the end of April.
             </p>
-
             <h2 className="title">12. ANNUAL SUBSCRIPTIONS</h2>
             <p>
               12.1. Annual subscriptions are due by 31 October, with a split
@@ -335,7 +358,6 @@ const Page = () => {
               12.4. The Committee will review annual subscription rates annually
               and communicate any changes to the club members.
             </p>
-
             <h2 className="title">13. COMMITTEE MEETINGS</h2>
             <p>
               13.1. Meetings of the Committee shall be held as often as
@@ -353,7 +375,6 @@ const Page = () => {
               select another member in their place until the next Annual General
               Meeting.
             </p>
-
             <h2 className="title">14. ANNUAL, GENERAL AND SPECIAL MEETINGS</h2>
             <p>
               14.1. The Annual General Meeting of the Club shall be held once a
@@ -361,7 +382,6 @@ const Page = () => {
               shall give at least (14) fourteen days&apos; notice of the date
               thereof to all members.
             </p>
-
             <h2 className="title">15. VOTING POWER</h2>
             <p>
               15.1. The Committee members shall have one (1) vote each, except
@@ -373,14 +393,17 @@ const Page = () => {
               represented by their parent or legal guardian.
             </p>
             <p>
-              15.3. No member shall be allowed to vote at any meeting of the
+              15.3. No single person shall be allowed to directly or indirectly
+              control the decision-making powers relating to the Club.
+            </p>
+            <p>
+              15.4. No member shall be allowed to vote at any meeting of the
               Club unless they are a fully paid up member.
             </p>
             <p>
-              15.4. Details of the Election of Committee Members process is
+              15.5. Details of the Election of Committee Members process is
               published separately.
             </p>
-
             <h2 className="title">
               16. BUSINESS AT THE ANNUAL GENERAL MEETING
             </h2>
@@ -414,20 +437,19 @@ const Page = () => {
               <li>To discuss any other business - general.</li>
               <li>To close the meeting.</li>
             </ul>
-
             <h2 className="title">17. QUORUM</h2>
             <p>
               A quorum for the Annual General Meeting shall be a minimum of 33%
               of fully paid up members eligible to vote. The Quorum can be
-              achieved either in person or digitally or a combination of both [1].
+              achieved either in person or digitally or a combination of both
+              [1].
             </p>
-
             <p id="">
               <small>
-                [1] Quorum amended Thursday 13 April 2023 to reflect the vote to amend. See minutes of 2023 AGM.
+                [1] Quorum amended Thursday 13 April 2023 to reflect the vote to
+                amend. See minutes of 2023 AGM.
               </small>
             </p>
-
             <h2 className="title">18. SPECIAL GENERAL MEETINGS</h2>
             <p>
               A Special General Meeting of the Club shall be called by the
@@ -435,7 +457,6 @@ const Page = () => {
               delivered to the Secretary of the Club signed by 75% of the
               members.
             </p>
-
             <h2 className="title">
               19. PROCEDURE AT A SPECIAL GENERAL MEETING
             </h2>
@@ -451,24 +472,51 @@ const Page = () => {
             <p>19.4. Closure.</p>
 
             <h2 className="title">
-              20. CLOSURE OF SOMERSET STRIKERS CRICKET CLUB
+              20. ACTIVITIES OF CLUB TO BE CARRIED ON IN NON-PROFIT MANNER
             </h2>
             <p>
-              Should the Club cease to exist then all monies and assets shall be
-              donated to any registered charity or organisation chosen by the
-              last elected Committee members of the Somerset Strikers Cricket
-              Club.
+              20.1 The activities of the Club may not be conducted with a
+              profit-making intention. This clause does not prevent the Club
+              from accumulating a financial surplus to achieve the Club’s
+              objectives set out in clause 3.
+            </p>
+            <p>
+              20.2 The Club may not pay remuneration to any person, which is
+              excessive having regard to remuneration which is generally
+              reasonable for the service delivered. Nor may the remuneration be
+              determined as a percentage of the Club’s income or surplus.
+            </p>
+
+            <h2 className="title">21. PROHIBITION OF DISTRIBUTIONS</h2>
+            <p>
+              The Club is prohibited from making any direct or indirect
+              distributions to any person other than in terms of clause 22
+              below.
             </p>
 
             <h2 className="title">
-              21. ADOPTION OF THE CONSTITUTION, RULES AND BY-LAWS
+              22. CLOSURE OF SOMERSET STRIKERS CRICKET CLUB
+            </h2>
+            <p>
+              22.1. Should the Club cease to exist then all monies and assets
+              shall be donated to any registered charity or organisation chosen
+              by the last elected Committee members of the Somerset Strikers
+              Cricket Club.
+            </p>
+            <p>
+              22.2. The registered charity or organisation that is chosen in
+              accordance with 20.1 is required to be an entity listed in section
+              30A(2)(a)(iii) of the Income Tax Act No. 58 of 1962 (as amended)
+              (“the IT Act”).
+            </p>
+            <h2 className="title">
+              23. ADOPTION OF THE CONSTITUTION, RULES AND BY-LAWS
             </h2>
             <p>
               The aforementioned articles, rules and by-laws of the Constitution
               have been accepted by the Somerset Strikers founding members.
             </p>
-
-            <h2 className="title">22. OUR MISSION STATEMENT</h2>
+            <h2 className="title">24. OUR MISSION STATEMENT</h2>
             <p>
               Somerset Strikers is an inclusive club that strives to include
               players of all abilities. Our primary goal is to encourage a team
@@ -478,10 +526,26 @@ const Page = () => {
               play cricket in a sportsmanlike fashion, giving due respect to the
               game and its traditions.
             </p>
+
+            <h2 className="title">25. FURTHER UNDERTAKINGS</h2>
+            <p>
+              25.1 The Club undertakes to submit a copy of any amendment to its
+              Constitution to the Commissioner of SARS.
+            </p>
+            <p>
+              25.2 The Club will further not permit itself to be used as part of
+              any transaction, operation or scheme of which the sole or main
+              purpose is or was the reduction, postponement or avoidance of
+              liability for any tax, duty or levy, which, but for that
+              transaction, operation or scheme, would have been or would have
+              become payable by any person under the IT Act or any other Act
+              administered by the Commissioner.{" "}
+            </p>
+
             <p className="has-text-centered">
               Committee: Wesley Moodie (Chairperson), Paul Watson (Vice
-              Chairperson), Fiona Haughney (Honorary Secretary), Leonard Willemse
-              (Honorary Treasurer), Jimmy Sounes (Club Captain)
+              Chairperson), Fiona Haughney (Honorary Secretary), Leonard
+              Willemse (Honorary Treasurer), Jimmy Sounes (Club Captain)
             </p>
           </section>
         </div>
