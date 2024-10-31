f539dd00c09f7b65992680e021b41c076e4412e0
paulmwatson <github.com@paulmwatson.com>
2024-10-31 12:33:30 +0200

    U10 rotation and guide

--- cypress/e2e/fixtures.cy.js
+++ cypress/e2e/fixtures.cy.js
@@ -3,6 +3,6 @@ import common from "/public/locales/en/common.json";
 describe("fixtures spec", () => {
   it("passes", () => {
     cy.visit("/fixtures").contains(common.pages.play.fixtures);
-    cy.location("pathname").should("eq", "/play/western-province-youth-league-2024-t1/u10/fixtures");
+    cy.location("pathname").should("eq", "/play/western-province-youth-league-2024-t4/u10/fixtures");
   });
 });
